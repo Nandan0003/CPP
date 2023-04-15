fff3af355ea96a115900857c4a433e172e598023
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-04-15 16:08:04 +0530

    ...

--- Adding nos.cpp
+++ Adding nos.cpp
@@ -8,17 +8,12 @@ int main()
   int a,b, sum;
 
 
-
   cout << "Enter two integers: "<<endl;
 
   cin>> "enter a " >> a; >> endl;
   cin>> "enter b " >> b; >> endl;
 
-
-
   sum = a + b;
-
- 
 
   cout << "sum of two nos is "<< sum <<endl;
 
