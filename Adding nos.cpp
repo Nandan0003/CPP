470ad5169a7a6fb2d3d86221cd3ed6bce50bb143
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-04-15 15:53:53 +0530

    Created adding of two nos cpp

---
+++ Adding nos.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+  int a,b, sum;
+
+
+
+  cout << "Enter two integers: "<<endl;
+
+  cin>> a >> b;
+
+
+
+  sum = a + b;
+
+ 
+
+  cout << "sum of two nos is "<< sum <<endl;
+
+  return 0;
+
+}
