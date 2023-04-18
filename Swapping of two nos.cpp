d7b1db353011048277643156449f5b26acbb385e
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-04-18 23:35:47 +0530

    Corrected errors

--- Swapping of two nos.cpp
+++ Swapping of two nos.cpp
@@ -10,14 +10,14 @@ cin>> a ;
 cout<<" enter value of b"<<endl;
 cin>> b;
 
-cout << "values  of a and b before swap"<<endl:
-cout << a << "&" << b << endl;
+cout << "values  of a and b before swap"<<endl;
+cout << a " "<< "&" <<" " b << endl;
 
 temp = a;
 a = b ;
 b = temp;
 
 cout << " Values of a and b after swapping" << endl;
-cout << a << "&" << b << endl;
-return ()
+cout << a " "<< "&" << " "b << endl;
+return 0;
 }
