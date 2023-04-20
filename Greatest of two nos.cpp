181529896274ddc577b05c69dbbc3d46f6508ed2
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-04-21 00:48:57 +0530

    1st commit

---
+++ Greatest of two nos.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+{
+int a,b;
+
+cout<<"enter a"<< endl;
+cin>> a ;
+cout<<"enter b"<<endl;
+cin>> b;
+
+int main()
+{
+
+if (a>b)
+{
+cout<<" a is greater "<< endl;
+}
+else 
+{
+cout<<" b is greater" <<endl;
+}
+
+return 0;
+}
