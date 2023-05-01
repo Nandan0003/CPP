d95a886e8d232c3060c73dbc02ab5b152285dbfa
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-05-01 22:55:04 +0530

    3

--- Factorial.cpp
+++ Factorial.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 int main()
 {
-int i,fact=1,no;
+int i,no;
+int fact=1;
 
 cout<<" enter a no"<< endl;
 cin>> no;
 
 for(i=0;i<=no;i++)
 {
-fact = fact * i;
+  fact = fact * i;
 }
 
-cout<<"factorial of " << no << "is" << fact<<endl;
+cout<<"factorial of " << no << "is" << fact <<endl;
 return 0;
 }
