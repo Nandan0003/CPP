649fe85dee3d2ffb71a5271243293196038982a9
Nandan0003 <118521395+Nandan0003@users.noreply.github.com>
2023-05-01 22:35:44 +0530

    Initial

---
+++ Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,fact=1,no;
+
+cout<<" enter a no<< endl;
+cin>> no;
+{
+for(i=0,i<=no,i++)
+fact = fact * 1;
+}
+
+cout<<"factorial of no" << no << "is" << fact<<endl;
+return 0;
+}
